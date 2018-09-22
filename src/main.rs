ce4bab967c9f8edc55f2450ac74bbc14dae967cd
Tom Pusateri <pusateri@bangj.com>
2018-09-22 07:39:46 -0400

    look at some new shared code.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+extern crate socket2;
+extern crate futures;
+extern crate tokio_core;
+extern crate tokio_io;
+#[macro_use]
+extern crate lazy_static;
 
-use std::net::{UdpSocket, IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
-use socket2::{self, Socket};
+use std::io;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
+use socket2::{Socket, Domain, Type, Protocol};
+use futures::{Future, Stream};
+use tokio_core::net::{UdpCodec, UdpSocket};
+use tokio_core::reactor::Core;
 
 pub const MDNS_PORT: u16 = 5353;
 lazy_static! {
@@ -10,17 +20,62 @@ lazy_static! {
     pub static ref MDNS_IPV6: SocketAddr = SocketAddr::new(Ipv6Addr::new(0xFF02, 0, 0, 0, 0, 0, 0, 0x00FB).into(), MDNS_PORT);
 }
 
+
+// just a codec to send and receive bytes
+pub struct LineCodec;
+impl UdpCodec for LineCodec {
+    type In = (SocketAddr, Vec<u8>);
+    type Out = (SocketAddr, Vec<u8>);
+
+    fn decode(&mut self, addr: &SocketAddr, buf: &[u8]) -> std::io::Result<Self::In> {
+        Ok((*addr, buf.to_vec()))
+    }
+
+    fn encode(&mut self, (addr, buf): Self::Out, into: &mut Vec<u8>) -> SocketAddr {
+        into.extend(buf);
+        addr
+    }
+}
+
+fn compute(handle: &Handle, addr: SocketAddr, _msg: Vec<u8>) -> Box<Future<Item = (), Error = ()>> {
+    println!("Starting to compute for: {}", addr);
+    Box::new(
+        Timeout::new(std::time::Duration::from_secs(8), handle)
+            .unwrap()
+            .map_err(|e| panic!("timeout failed: {:?}", e))
+            .and_then(move |()| {
+                println!("Done computing for for: {}", addr);
+                Ok(())
+            }),
+    )
+}
+
 fn main() {
-    let res = join_multicast(MDNS_IPV4);
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+    let socket = join_multicast(&MDNS_IPV4).expect("mDNS IPv4 join_multicast");
+
+    let (writer, reader) = socket.framed(LineCodec).split();
+
+    let socket_read = reader.for_each(|(addr, msg)| {
+        println!("Got {:?}", msg);
+        handle.spawn(compute(addr, msg));
+        Ok(())
+    });
+
+    core.run(socket_read).unwrap();
+}
+
+
+#[cfg(unix)]
+fn bind_multicast(socket: &Socket, addr: &SocketAddr) -> io::Result<()> {
+    socket.bind(&socket2::SockAddr::from(*addr))
 }
 
 /// Returns a socket joined to the multicast address
 fn join_multicast(
     multicast_addr: &SocketAddr,
 ) -> Result<Option<std::net::UdpSocket>, io::Error> {
-    if !mdns_query_type.join_multicast() {
-        return Ok(None);
-    }
 
     let ip_addr = multicast_addr.ip();
     // it's an error to not use a proper mDNS address
@@ -37,33 +92,32 @@ fn join_multicast(
     let socket = match ip_addr {
         IpAddr::V4(ref mdns_v4) => {
             let socket = Socket::new(
-                socket2::Domain::ipv4(),
-                socket2::Type::dgram(),
-                Some(socket2::Protocol::udp()),
-            )?;
-            socket.join_multicast_v4(mdns_v4, &Ipv4Addr::new(0, 0, 0, 0))?;
+                Domain::ipv4(),
+                Type::dgram(),
+                Some(Protocol::udp()),
+            ).expect("ipv4 dgram socket");
+            socket.join_multicast_v4(mdns_v4, &Ipv4Addr::new(0, 0, 0, 0)).expect("join_multicast_v4");
             socket
         }
         IpAddr::V6(ref mdns_v6) => {
             let socket = Socket::new(
-                socket2::Domain::ipv6(),
-                socket2::Type::dgram(),
-                Some(socket2::Protocol::udp()),
-            )?;
+                Domain::ipv6(),
+                Type::dgram(),
+                Some(Protocol::udp()),
+            ).expect("ipv6 dgram socket");
 
             socket.set_only_v6(true)?;
-            socket.join_multicast_v6(mdns_v6, 0)?;
+            socket.join_multicast_v6(mdns_v6, 0).expect("join_multicast_v6");
             socket
         }
     };
 
-    socket.set_nonblocking(true)?;
-    socket.set_reuse_address(true)?;
+    socket.set_nonblocking(true).expect("nonblocking Error");
+    socket.set_reuse_address(true).expect("reuse addr Error");
     #[cfg(unix)] // this is currently restricted to Unix's in socket2
-    socket.set_reuse_port(true)?;
-    Self::bind_multicast(&socket, multicast_addr)?;
+    socket.set_reuse_port(true).expect("reuse port Error");
+    bind_multicast(&socket, &multicast_addr).expect("bind Error");
 
-    debug!("joined {}", multicast_addr);
     Ok(Some(socket.into_udp_socket()))
 }
 
