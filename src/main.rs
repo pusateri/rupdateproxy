2af9e79fecfa407429325296d39cf7a397677f21
Tom Pusateri <pusateri@bangj.com>
2019-03-27 18:35:37 -0400

    Switch from tokio to mio and mpsc to crossbeam.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::update::UpdateServer;
+
 use crate::services::{ServiceAction, ServiceEvent};
 use bytes::Bytes;
 use domain_core::bits::message::Message;
@@ -7,22 +7,19 @@ use domain_core::bits::Dname;
 use domain_core::bits::record::Record;
 use domain_core::rdata::AllRecordData;
 use domain_resolv::StubResolver;
-use futures::sync::mpsc;
 use interface_events::{get_current_events, IfEvent};
 use lazy_static::lazy_static;
 use socket2::Domain;
+use std::thread;
 use std::sync::{Arc, Mutex};
 use std::collections::hash_map::Entry::{Occupied, Vacant};
 use std::collections::HashMap;
-use std::error::Error;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4};
 use std::process::exit;
 use std::time::{Duration, Instant};
-use tokio::net::{UdpFramed, UdpSocket};
-use tokio::prelude::*;
-use tokio::runtime::Runtime;
-use tokio::timer::Delay;
-use tokio_codec::BytesCodec;
+use mio::net::UdpSocket;
+use mio::{Events, Ready, Poll, PollOpt, Token};
+use crossbeam_channel::{unbounded, Sender};
 use treebitmap;
 
 mod args;
@@ -37,8 +34,6 @@ lazy_static! {
     pub static ref MDNS_IPV4: SocketAddr = SocketAddr::new(Ipv4Addr::new(224,0,0,251).into(), MDNS_PORT);
     /// link-local mDNS ipv6 address https://www.iana.org/assignments/ipv6-multicast-addresses/ipv6-multicast-addresses.xhtml
     pub static ref MDNS_IPV6: SocketAddr = SocketAddr::new(Ipv6Addr::new(0xFF02, 0, 0, 0, 0, 0, 0, 0x00FB).into(), MDNS_PORT);
-
-    static ref USMAP: HashMap<String, Arc<Mutex<UpdateServer>>> = HashMap::new();
 }
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
@@ -143,15 +138,15 @@ fn extract_mdns_response(
     subdomain: &String,
     from: SocketAddr,
     buf: &[u8],
-    tx: &mut mpsc::Sender<services::ServiceEvent>,
-) -> Result<(), Box<Error>> {
+    tx: &mut Sender<services::ServiceEvent>,
+) {
     let msg = Message::from_bytes(Bytes::from(buf)).expect("DNS Message::from_bytes failed");
 
     if msg.is_error() {
-        return Ok(());
+        return;
     }
     if msg.header().qr() == false {
-        return Ok(());
+        return;
     }
 
     for section in vec![msg.answer(), msg.additional()] {
@@ -164,14 +159,13 @@ fn extract_mdns_response(
                     let se = extract_mdns_record(ifindex, subdomain, from, r);
                     if let Some(event) = se {
                         // send over channel combining IPv4 and IPv6 received mDNS messages
-                        tx.send(event).wait().unwrap();
+                        tx.send(event).unwrap();
                     }
                 },
                 Err(_e) => (),
             }
         }
     }
-    Ok(())
 }
 
 fn ifstate_for_v4_address(
@@ -247,28 +241,27 @@ fn interface_trees_init(
 fn update_servers_init(
     v4_ifs: &treebitmap::IpLookupTable<std::net::Ipv4Addr, IfState>,
     v6_ifs: &treebitmap::IpLookupTable<std::net::Ipv6Addr, IfState>,
-) {
+) -> HashMap <String, Arc<Mutex<update::UpdateServer>>>{
+    let mut usmap = HashMap::new();
     for (_addr, _plen, intf) in v4_ifs.iter() {
         let up = update::UpdateServer::new(Domain::ipv4(), intf.subdomain.clone());
-        //let mut servers = USMAP.lock().unwrap();
-        USMAP.insert(intf.subdomain.clone(), Arc::new(Mutex::new(up)));
+        usmap.insert(intf.subdomain.clone(), Arc::new(Mutex::new(up)));
     }
     for (_addr, _plen, intf) in v6_ifs.iter() {
         let up = update::UpdateServer::new(Domain::ipv6(), intf.subdomain.clone());
-        //let mut servers = USMAP.lock().unwrap();
-        USMAP.insert(intf.subdomain.clone(), Arc::new(Mutex::new(up)));
+        usmap.insert(intf.subdomain.clone(), Arc::new(Mutex::new(up)));
     }
+    usmap
 }
 
 fn build_update(se: &services::ServiceEvent) -> Message
 {
-    println!("sending msg: {}", se.sname);
     use std::str::FromStr;
     use domain_core::bits::{Dname, MessageBuilder, SectionBuilder, RecordSectionBuilder};
     use domain_core::iana::opcode;
     use domain_core::iana::Rtype;
 
-    let mut msg = MessageBuilder::new_udp();
+    let mut msg = MessageBuilder::with_capacity(4096);
     msg.header_mut().set_opcode(opcode::Opcode::Update);
 
     // Zone section
@@ -346,150 +339,99 @@ fn main() {
 
     // create a mapping from subdomain name to update server
     // TODO: periodically refresh
-    update_servers_init(&v4_ifs, &v6_ifs);
+    let usmap = update_servers_init(&v4_ifs, &v6_ifs);
 
     // create channel for ServiceEvents
-    let (tx, rx) = mpsc::channel(1000);
+    let (tx, rx) = unbounded::<services::ServiceEvent>();
 
     // create cache receiver for ServiceEvents
     // create a services cache per interface index, IPv4 & IPv6 should be merged
     let mut cache_map: HashMap<u32, HashMap<RecordKey, RecordInfo>> = HashMap::new();
 
     // receive mDNS events over channel
-    let service_sink = rx.for_each(move |se: services::ServiceEvent| {
-        if !cache_map.contains_key(&se.ifindex) {
-            let table = HashMap::new();
-            cache_map.insert(se.ifindex, table);
-        }
-        let cache = cache_map.get_mut(&se.ifindex).unwrap();
-        let when = Instant::now() + Duration::from_secs(se.ttl.into());
+    thread::spawn(move || {
+        for se in rx.iter() {
+            if !cache_map.contains_key(&se.ifindex) {
+                let table = HashMap::new();
+                cache_map.insert(se.ifindex, table);
+            }
+            let cache = cache_map.get_mut(&se.ifindex).unwrap();
+            let when = Instant::now() + Duration::from_secs(se.ttl.into());
 
-        let key = RecordKey {
-            name: se.sname.clone(),
-            data: se.sdata.clone(),
-        };
-        let val = RecordInfo { ttl: se.ttl };
+            let key = RecordKey {
+                name: se.sname.clone(),
+                data: se.sdata.clone(),
+            };
+            let val = RecordInfo { ttl: se.ttl };
 
-        match cache.entry(key.clone()) {
-            Vacant(entry) => {
-                println!(
-                    "caching {} + {:?} on ifindex: {}, subdomain: {}",
-                    key.name, key.data, se.ifindex, se.subdomain,
-                );
-                let task = Delay::new(when)
-                    .map_err(|e| panic!("delay errored; err={:?}", e))
-                    .and_then(move |_| {
-                        println!("timeout for {} + {:?}", key.name, key.data);
-                        Ok(())
-                    });
+            match cache.entry(key.clone()) {
+                Vacant(entry) => {
+                    println!(
+                        "caching {} + {:?} on ifindex: {}, subdomain: {}",
+                        key.name, key.data, se.ifindex, se.subdomain,
+                    );
+                    /* TODO: 
+                    let task = Delay::new(when)
+                        .map_err(|e| panic!("delay errored; err={:?}", e))
+                        .and_then(move |_| {
+                            println!("timeout for {} + {:?}", key.name, key.data);
+                            Ok(())
+                        });
 
-                tokio::spawn(task);
-                entry.insert(val);
+                    tokio::spawn(task);
+                    */
+                    entry.insert(val);
 
-                // send new cache entries to DNS Update server.
-                if let Some(ups) = USMAP.get(&se.subdomain) {
-                    update::send(ups, build_update(&se));
+                    // send new cache entries to DNS Update server.
+                    if let Some(ups) = usmap.get(&se.subdomain) {
+                        update::send(ups, build_update(&se));
+                    }
+                    
                 }
-                
-            }
-            Occupied(exists) => {
-                println!(
-                    "found: {} + {:?} on ifindex: {}, subdomain {}",
-                    key.name, key.data, se.ifindex, se.subdomain,
-                );
-                let mut entry = exists.into_mut();
-                entry.ttl = se.ttl;
-            }
-        };
-        Ok(())
+                Occupied(exists) => {
+                    println!(
+                        "found: {} + {:?} on ifindex: {}, subdomain {}",
+                        key.name, key.data, se.ifindex, se.subdomain,
+                    );
+                    let mut entry = exists.into_mut();
+                    entry.ttl = se.ttl;
+                }
+            };
+        }
     });
 
-    // listen for IPv4 mDNS packets
+    const IPV4MC_ALLIF: Token = Token(0);
+    let poll = Poll::new().expect("Poll::new() failed");
+
     let v4_listen_addr = SocketAddr::from(SocketAddrV4::new(IP_ALL.into(), MDNS_PORT));
-    let v4_socket_read =
-        if v4_ifs.len() > 0 {
-            let v4_std_socket = multicast::join_multicast(&MDNS_IPV4, &v4_listen_addr, 0)
-                .expect("mDNS IPv4 join_multicast");
-            let v4_socket = UdpSocket::from_std(v4_std_socket, &tokio::reactor::Handle::default()).unwrap();
-            let (_v4_writer, v4_reader) = UdpFramed::new(v4_socket, BytesCodec::new()).split();
-
-            let mut source = tx.clone();
-            Some(v4_reader.for_each(move |(msg, addr)| {
-                match ifstate_for_v4_address(addr, &v4_ifs) {
-                    Some(ifs) => match extract_mdns_response(ifs.ifindex, &ifs.subdomain, addr, &msg, &mut source) {
-                        Ok(()) => {}
-                        Err(e) => {
-                            eprintln!("Error from {}: {}", addr, e);
-                        }
-                    },
-                    None => {
-                        eprintln!("No interface for addr {:?}", addr);
-                    }
-                };
-
-                Ok(())
-            }))
-        } else {
-            None
-        };
-
-    // listen for IPv6 mDNS packets
-    let v6_socket_read =
-        if v6_ifs.len() > 0 {
-            let v6_std_socket = multicast::join_multicast(&MDNS_IPV6, &v4_listen_addr, 0)
-                .expect("mDNS IPv6 join_multicast");
-            let v6_socket = UdpSocket::from_std(v6_std_socket, &tokio::reactor::Handle::default()).unwrap();
-            let (_v6_writer, v6_reader) = UdpFramed::new(v6_socket, BytesCodec::new()).split();
-            let mut source = tx.clone();
-
-            Some(v6_reader.for_each(move |(msg, addr)| {
-                match ifstate_for_v6_address(addr, &v6_ifs) {
-                    Some(ifs) => match extract_mdns_response(ifs.ifindex, &ifs.subdomain, addr, &msg, &mut source) {
-                        Ok(()) => {}
-                        Err(e) => {
-                            eprintln!("Error from {}: {}", addr, e);
-                        }
-                    },
-                    None => {
-                        eprintln!("No interface for addr {:?}", addr);
-                    }
-                };
-
-                Ok(())
-            }))
-        } else {
-            None
-        };
-
-    let mut rt = Runtime::new().unwrap();
-
-    // Spawn the server tasks
+    let v4_std_socket = multicast::join_multicast(&MDNS_IPV4, &v4_listen_addr, 0)
+                                .expect("mDNS IPv4 join_multicast");
+    let v4_socket = UdpSocket::from_socket(v4_std_socket).expect("mio from_socket()");
     if options.nofour == false {
-        if let Some(reader) = v4_socket_read {
-            rt.spawn(
-                reader
-                    .map(|_| ())
-                    .map_err(|_| eprintln!("v4_socket_read")),
-            );
-        } else {
-            eprintln!("IPv4 is enabled but no addresses are usable");
-        }
+        poll.register(&v4_socket, IPV4MC_ALLIF, Ready::readable(), PollOpt::edge()).expect("poll.register failed");
     }
 
-    if options.nosix == false {
-        if let Some(reader) = v6_socket_read {
-            rt.spawn(
-                reader
-                    .map(|_| ())
-                    .map_err(|_| eprintln!("v6_socket_read")),
-            );
-        } else {
-            eprintln!("IPv6 is enabled but no addresses are usable");
+    let mut events = Events::with_capacity(1024);
+    let mut source = tx.clone();
+    let mut buf = [0; 4096];
+    loop {
+        poll.poll(&mut events, None).expect("poll.poll failed");
+        for event in events.iter() {
+            match event.token() {
+                IPV4MC_ALLIF => {
+                    let (_length, from_addr) = v4_socket.recv_from(&mut buf).expect("recv_from failed");
+                    //println!("event from {:?}", from_addr);
+                    match ifstate_for_v4_address(from_addr, &v4_ifs) {
+                        Some(ifs) => {
+                            extract_mdns_response(ifs.ifindex, &ifs.subdomain, from_addr, &buf, &mut source);
+                        },
+                        None => {
+                            eprintln!("No interface for addr {:?}", from_addr);
+                        }
+                    };
+                }
+                _ => unreachable!()
+            }
         }
     }
-    
-    rt.spawn(service_sink);
-
-    // Wait until the runtime becomes idle and shut it down.
-    rt.shutdown_on_idle().wait().unwrap();
 }
--- src/update.rs
+++ src/update.rs
@@ -1,11 +1,5 @@
-#![feature(optin_builtin_traits)]
-
 use socket2::Domain;
 use std::sync::{Arc, Mutex};
-use futures::stream::{SplitSink, SplitStream};
-use tokio::prelude::*;
-use tokio::net::{UdpFramed, UdpSocket};
-use tokio_codec::BytesCodec;
 use std::net::SocketAddr;
 use std::net::Ipv4Addr;
 use domain_core::bits::message::Message;
@@ -21,24 +15,13 @@ const DNS_PORT: u16 = 53;
 
 pub struct UpdateServer {
 	subdomain: String,
-    sa: SocketAddr,
-    sink: Arc<Mutex<SplitSink<UdpFramed<BytesCodec>>>>,
-    stream: Arc<Mutex<SplitStream<UdpFramed<BytesCodec>>>>,
 }
-
-unsafe impl Send for UpdateServer {}
-unsafe impl Sync for UpdateServer {}
 
 impl UpdateServer {
     pub fn new(_family: Domain, subdomain: String) -> Self {
-    	let addr = SocketAddr::new(Ipv4Addr::new(127,0,0,1).into(), 8053);
-    	let sock = UdpSocket::bind(&addr).expect("bind failed");
-    	let (a_sink, a_stream) = UdpFramed::new(sock, BytesCodec::new()).split();
+    	//let addr = SocketAddr::new(Ipv4Addr::new(127,0,0,1).into(), 8053);
         UpdateServer {
         	subdomain: subdomain,
-            sa: addr,
-            sink: Arc::new(Mutex::new(a_sink)),
-            stream: Arc::new(Mutex::new(a_stream)),
         }
     }
 }
@@ -49,24 +32,10 @@ pub fn resolve_server(_domain: String) -> SocketAddr
 	SocketAddr::new(Ipv4Addr::new(127,0,0,1).into(), DNS_PORT)
 }
 
-pub fn send(uswrap: &Arc<Mutex<UpdateServer>>, msg: Message)
+pub fn send(uswrap: &Arc<Mutex<UpdateServer>>, _msg: Message)
 {
-	let us = uswrap.lock().unwrap();
-	let a_sink = us.sink.lock().unwrap();
-    let task = a_sink.send((msg.as_bytes().clone(), us.sa))
-    	.and_then(move |_| {
-    		let a_stream = us.stream.lock().unwrap();
-			let _a_stream = a_stream.take(1)
-        	.into_future()
-        	.map(move |(_response, addr)| {
-            	println!("task recv from {:?}", addr);
-        	})
-        	.map_err(|e| panic!("send update err={:?}", e));
-        	Ok(())
-    	})
-    	.or_else(|n| {
-            println!("read {:?} bytes2", n);
-            Ok(())
-        });
-    tokio::spawn(task);
+    let _result = { 
+        let _us = uswrap.lock().unwrap();
+    };
+    
 }
